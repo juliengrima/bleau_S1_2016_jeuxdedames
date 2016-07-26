dce7780fe66bac17bfb1eca5c9977b64eb514200
Florian Grandjean <florian.pdf@gmail.com>
2016-07-26 15:47:32 +0200

    mise en page front artiste index

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('UserBundle:Default:index.html.twig');
-    }
+//    public function indexAction()
+//    {
+//        return $this->render('UserBundle:Default:index.html.twig');
+//    }
 }
